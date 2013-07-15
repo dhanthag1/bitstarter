7f2c2071adba1eb2619694f3c979a888618d52fa
Dhantha <dgunarat@ramapo.edu>
2013-07-15 14:28:03 +0000

    Added index.html

--- web.js
+++ web.js
@@ -1,12 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 var app = express.createServer(express.logger());
 
-app.get('/',function(request,response) {
-var fs = require('fs');
-var buffer = new Buffer (256);
-response.send(fs.readFileSync('index.html').toString());
+app.get('/', function(request,response) {
+var html = fs.readFileSync(htmlfile).toString();
+response.send(html);
 });
-web: node app.js
+
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
