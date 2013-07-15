bbf82b8e89c282e4d4d150108b2d7335b8f4a929
Dhantha <dgunarat@ramapo.edu>
2013-07-15 03:11:22 +0000

    Added index.html

--- web.js
+++ web.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var buffer = Buffer(128);
-buffer = fs.readFileSync('index.html');
-var fs = require('fs');
+buffer = fs.readFileSync("index.html");
+var fs = require("fs");
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
