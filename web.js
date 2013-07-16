eaa6de818a5fde71d6e7a665d7dd61cb27f0e1ca
Dhantha <dgunarat@ramapo.edu>
2013-07-16 14:14:19 +0000

    Added index.html

--- web.js
+++ web.js
@@ -8,7 +8,7 @@ var html = fs.readFileSync(htmlfile).toString();
 response.send(html);
 });
 
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
